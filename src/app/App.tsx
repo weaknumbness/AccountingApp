23876308ee6fad706a0e73dec8cd59c37f233e6d
weaknumbness <sergey.stepanov.stud@mail.ru>
2026-06-23 18:48:42 +0300

    new AsideBar without adaptive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,44 +1,46 @@
-import { useEffect, useState } from "react";
-import { onAuthStateChanged, signOut, type User } from "firebase/auth";
-import { auth } from "../services/firebase/firebase";
-import { AuthPage } from "../features/auth/AuthPage";
-import { AppHeader } from "../components/layout/AppHeader";
+// import { useEffect, useState } from "react";
+// import { onAuthStateChanged, signOut, type User } from "firebase/auth";
+// import { auth } from "../services/firebase/firebase";
+// import { AuthPage } from "../features/auth/AuthPage";
+// import { AppHeader } from "../components/layout/AppHeader";
 import "../styles/reset.css";
-import "../styles/style.css";
+import "../styles/newStyle.css";
+import AppSideBar from "../components/layout/AppSideBar";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
-  const [isAuthLoading, setIsAuthLoading] = useState(true);
+  // const [user, setUser] = useState<User | null>(null);
+  // const [isAuthLoading, setIsAuthLoading] = useState(true);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setIsAuthLoading(false);
-    });
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //     setUser(currentUser);
+  //     setIsAuthLoading(false);
+  //   });
 
-    return unsubscribe;
-  }, []);
+  //   return unsubscribe;
+  // }, []);
 
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
+  // const handleLogout = async () => {
+  //   await signOut(auth);
+  // };
 
-  if (isAuthLoading) {
-    return <div>Загрузка...</div>;
-  }
+  // if (isAuthLoading) {
+  //   return <div>Загрузка...</div>;
+  // }
 
-  if (!user) {
-    return <AuthPage />;
-  }
+  // if (!user) {
+  //   return <AuthPage />;
+  // }
 
   return (
     <div>
-      <AppHeader userEmail={user.email} onLogout={handleLogout} />
+      {/* <AppHeader userEmail={user.email} onLogout={handleLogout} />
 
       <main>
         <h2>Главный экран приложения</h2>
         <p>Авторизация восстановлена. Следующий шаг — категории.</p>
-      </main>
+      </main> */}
+      <AppSideBar/>
     </div>
   );
 }
--- src/components/layout/AppSideBar.tsx
+++ src/components/layout/AppSideBar.tsx
@@ -1,27 +1,158 @@
 import { motion } from "motion/react";
+import logo from "../assets/Logo.png";
 
 export default function AppSideBar() {
+  const User = {
+    pic: "src",
+    mail: "user@example.com",
+    nick: "Пользователь",
+  };
+
   return (
     <motion.div className="side-bar">
-      <div className="logo">
-        <div className="logo-picture"></div>
-        <h1>AccountingApp</h1>
-      </div>
-      <div className="menu-settings">
-        <div className="menu">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+      <div className="top-side">
+        <div className="logo">
+          <div className="logo-picture">
+            {/* Что то надо придумать с логотипом */}
+            <img src={logo} alt="" />
+          </div>
+          <h1>AccountingApp</h1>
         </div>
-        <div className="settings">
-          <div className="profile"></div>
-          <div className="exit"></div>
+        <div className="menu-settings">
+          <div className="menu">
+            <h2>МЕНЮ</h2>
+            <ul>
+              <motion.li whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }}>
+                <div className="li-picture">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-bag"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                  </svg>
+                </div>
+                Товары
+              </motion.li>
+              <motion.li whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }}>
+                <div className="li-picture">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-tags"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z" />
+                    <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z" />
+                  </svg>
+                </div>
+                Категории
+              </motion.li>
+              <motion.li whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }}>
+                <div className="li-picture">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-cash"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                    <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z" />
+                  </svg>
+                </div>
+                Продажи
+              </motion.li>
+              <motion.li whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }}>
+                <div className="li-picture">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-receipt"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z" />
+                    <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z" />
+                  </svg>
+                </div>
+                Отчёты
+              </motion.li>
+            </ul>
+          </div>
+          <div className="divider"></div>
+          <div className="settings">
+            <h2>НАСТРОЙКИ</h2>
+            <ul>
+              <motion.li whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }}>
+                <div className="li-picutre">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-person"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                  </svg>
+                </div>
+                Профиль
+              </motion.li>
+              <motion.li whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.0 }}>
+                <div className="li-picutre">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-box-arrow-left"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+                    />
+                  </svg>
+                </div>
+                Выйти
+              </motion.li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="user-profile"></div>
+      <div className="down-side">
+        <div className="user-profile">
+          {/* Сюда вставить аватарку пользователя */}
+          <div className="user-picture">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              fill="currentColor"
+              className="bi bi-person"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+            </svg>
+          </div>
+          <div className="user-info">
+            <div className="user-mail">{User.mail}</div>
+            <div className="name">{User.nick}</div>
+          </div>
+        </div>
+      </div>
     </motion.div>
   );
 }
